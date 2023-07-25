cad08d44134395fc25afeb8d08ba2b1530aae812
Sujan Mallikarjun <blacjac210@gmail.com>
2023-07-25 17:06:01 +0530

    navbar prototype

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,49 @@
 import Image from "next/image";
+import logo from '@/public/logo.png'
 
 const Navbar: React.FC = () => {
   return (
-    <div className="navbar bg-base-100">
-    <div className="navbar-start">
-      <div className="dropdown">
-        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div>
-      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-    </div>
-    <div className="navbar-center hidden lg:flex">
+    <div className="navbar bg-red-500">
+      <div className="navbar-start">
+        <div className="w-full sm:w-auto" >
+        <Image 
+        src={logo} 
+        alt="logo"
+        className="pl-4 pt-2 pb-2 pr-2"
+        />
+        </div>
+
+
+        <a className="btn btn-ghost normal-case text-xl sm:text-lg md:text-xl lg:text-2xl">Filcon Sales and Service</a>
+        </div>
+
+ 
+
+      
+
+      <div className="navbar-end">
+      <div className="hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
+      <li><a>Products</a></li>
+      <li><a>About Us</a></li>
+      <li><a>Contact Us</a></li>
+    </ul>
+      </div>
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a>Products</a></li>
-        <li><a>About Us</a></li>
+        <li>
+          <a>About Us</a>
+          
+        </li>
         <li><a>Contact Us</a></li>
       </ul>
     </div>
-  </div>
+    </div>
+    </div>
   );
 };
 
