24d38e82e4273090c6591320347a45666217a7bd
Sujan Mallikarjun <blacjac210@gmail.com>
2023-07-30 14:13:38 +0530

    main features

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Features from "@/components/Features"
 import Carousel from "@/components/carousel"
 import Navbar from "@/components/navbar"
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
     <Carousel />
+    <Features />
     
     </main>
   )
